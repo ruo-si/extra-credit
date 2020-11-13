2da3eb1dea18bc7119c7f81bdb1e36bcdb0c1872
Skyler R <skyler.rencher@gmail.com>
2020-11-13 15:50:35 -0500

    added the citiCard and jetBlueCard functions that calculate the perks for the cards.

--- models/calculate.js
+++ models/calculate.js
@@ -28,3 +28,63 @@ const amexCard = function (num1) {
 
 amexCard(400);
 
+const citiCard = function (num1) {
+    const cashBack = function (num1) {
+        const num2 = .02;
+        const result = num1 * num2;
+        console.log("$" + result)
+        return result;
+    }
+    cashBack(num1);
+
+    const rewardsEarned = function (num1) {
+        const num2 = .75;
+        const result = num1 * num2;
+        console.log(result + " points")
+        return result;
+    }
+    rewardsEarned(num1);
+
+    const milesEarned = function (num1) {
+        const num2 = 1.25;
+        const result = num1 * num2;
+        console.log(result + " miles")
+        return result;
+    }
+    milesEarned(num1);
+
+
+}
+
+citiCard(400);
+
+const jetBlueCard = function (num1) {
+    const cashBack = function (num1) {
+        const num2 = .02;
+        const result = num1 * num2;
+        console.log("$" + result)
+        return result;
+    }
+    cashBack(num1);
+
+    const rewardsEarned = function (num1) {
+        const num2 = .75;
+        const result = num1 * num2;
+        console.log(result + " points")
+        return result;
+    }
+    rewardsEarned(num1);
+
+    const milesEarned = function (num1) {
+        const num2 = 1.25;
+        const result = num1 * num2;
+        console.log(result + " miles")
+        return result;
+    }
+    milesEarned(num1);
+
+
+}
+
+jetBlueCard(400);
+
