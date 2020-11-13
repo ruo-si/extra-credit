d7904d665b7c0adcd19a306cef92468e78531c90
Skyler R <skyler.rencher@gmail.com>
2020-11-13 16:19:39 -0500

    added the .toFixed method to the results to only show the two decimal points after.

--- models/calculate.js
+++ models/calculate.js
@@ -2,24 +2,24 @@ const amexCard = function (num1) {
     const cashBack = function (num1) {
         const num2 = .02;
         const result = num1 * num2;
-        console.log("$" + result)
-        return result;
+        console.log("$" + result.toFixed(2))
+        return result.toFixed(2);
     }
     cashBack(num1);
 
     const rewardsEarned = function (num1) {
         const num2 = .75;
         const result = num1 * num2;
-        console.log(result + " points")
-        return result;
+        console.log(result.toFixed(2) + " points")
+        return result.toFixed(2);
     }
     rewardsEarned(num1);
 
     const milesEarned = function (num1) {
         const num2 = 1.25;
         const result = num1 * num2;
-        console.log(result + " miles")
-        return result;
+        console.log(result.toFixed(2) + " miles")
+        return result.toFixed(2);
     }
     milesEarned(num1);
 
@@ -30,26 +30,26 @@ amexCard(400);
 
 const citiCard = function (num1) {
     const cashBack = function (num1) {
-        const num2 = .02;
+        const num2 = .04;
         const result = num1 * num2;
-        console.log("$" + result)
-        return result;
+        console.log("$" + result.toFixed(2))
+        return result.toFixed(2);
     }
     cashBack(num1);
 
     const rewardsEarned = function (num1) {
-        const num2 = .75;
+        const num2 = .80;
         const result = num1 * num2;
-        console.log(result + " points")
-        return result;
+        console.log(result.toFixed(2) + " points")
+        return result.toFixed(2);
     }
     rewardsEarned(num1);
 
     const milesEarned = function (num1) {
-        const num2 = 1.25;
+        const num2 = 1.10;
         const result = num1 * num2;
-        console.log(result + " miles")
-        return result;
+        console.log(result.toFixed(2) + " miles")
+        return result.toFixed(2);
     }
     milesEarned(num1);
 
@@ -60,26 +60,26 @@ citiCard(400);
 
 const jetBlueCard = function (num1) {
     const cashBack = function (num1) {
-        const num2 = .02;
+        const num2 = .03;
         const result = num1 * num2;
-        console.log("$" + result)
-        return result;
+        console.log("$" + result.toFixed(2));
+        return result.toFixed(2);
     }
     cashBack(num1);
 
     const rewardsEarned = function (num1) {
-        const num2 = .75;
+        const num2 = .85;
         const result = num1 * num2;
-        console.log(result + " points")
-        return result;
+        console.log(result.toFixed(2) + " points");
+        return result.toFixed(2);
     }
     rewardsEarned(num1);
 
     const milesEarned = function (num1) {
-        const num2 = 1.25;
+        const num2 = 1.15;
         const result = num1 * num2;
-        console.log(result + " miles")
-        return result;
+        console.log(result.toFixed(2) + " miles");
+        return result.toFixed(2);
     }
     milesEarned(num1);
 
