ff87fb74a9b27a7729d68632717b89357723037d
Victoria Lee <victoriacmlee@gmail.com>
2020-11-14 11:15:04 -0500

    prep for development

--- db/schema.sql
+++ db/schema.sql
@@ -2,18 +2,18 @@ DROP DATABASE IF EXISTS test_db;
 CREATE DATABASE test_db;
 
 
-DROP DATABASE IF EXISTS extra_credit_db;
-CREATE DATABASE extra_credit_db;
+-- DROP DATABASE IF EXISTS extra_credit_db;
+-- CREATE DATABASE extra_credit_db;
 
-USE extra_credit_db;
+-- USE extra_credit_db;
 
 
--- SEARCH HISTORY --
-CREATE TABLE search_history(
-    id INT NOT NULL AUTO_INCREMENT,
-    search_price INTEGER,
-    PRIMARY KEY (id)
-);
+-- -- SEARCH HISTORY --
+-- CREATE TABLE search_history(
+--     id INT NOT NULL AUTO_INCREMENT,
+--     search_price INTEGER,
+--     PRIMARY KEY (id)
+-- );
 
 -- WALLET --
 
