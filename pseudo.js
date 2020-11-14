4a091bb81a63c2f63f3e6438ed9f2a93507f1843
Victoria Lee <victoriacmlee@gmail.com>
2020-11-13 23:01:07 -0500

    pasudo setup

---
+++ pseudo.js
@@ -0,0 +1,53 @@
+// user land on page (routes)
+
+// set up buttons with event listeners to make a selection
+
+//  =============================================================
+
+// sign up - customize wallet
+            // display form to fill out
+            // submit form and POST resquest to databse
+
+            // customize wallet
+            // display cards
+            // POST selected card to database
+        
+//  =============================================================
+
+        // rewards calculator
+            // GET wallet selection data from database and assign to selction array
+
+            // run through selection array
+                // let compare array = [ ]
+
+                // switch statement
+                    // case card_name 1
+                        // run function card1
+                        // push response to compare array
+
+                    // case card_name 2
+                        // run function card1
+                        // push response to compare array
+
+                    // case card_name 3
+                        // run function card3
+                        // push response to compare array
+
+            // render response to handlebar
+
+
+// function card 1 calculation
+// function card 2 calculation
+// function card 3 calculation
+
+
+// render results to page
+
+
+//  =============================================================
+
+// pull a random fact from facts database
+    // GET request for a random fact with math.random(fact.id) from database
+    // render res.params.fact to handlebar
+
+
