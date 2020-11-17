3183c7f6fcc639c50588ec5be1764a2b35097855
Skyler R <skyler.rencher@gmail.com>
2020-11-17 02:03:12 -0500

    added a function that listens to a click event that initiates the card functions in calculate.js.

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -13,7 +13,7 @@ $("#fun-fact-btn").on("click", () => {
       type: "GET",
    }).then(
       (data) => {
-         console.log(data)
+         console.log(data);
          // set up appending data
          const factContent = `<p><mark><strong>Fact: ${data.id } : </strong></mark> ${data.fact}</p>`;
  
@@ -26,4 +26,45 @@ $("#fun-fact-btn").on("click", () => {
          location.reload;
       }
    );
+});
+
+$(userSpendBtn).on("click", (event) => {
+   event.preventDefault();
+
+
+
+   // This line grabs the input from the textbox
+   const userSpendInput = $("#user-spend-input").val();
+
+
+
+   console.log(`money spent: ${userSpendInput}`);
+   spendAmountSaved.push(userSpendInput);
+   console.log(spendAmountSaved);
+
+   localStorage.setItem("spentAmount", JSON.stringify(spendAmountSaved));
+
+
+   const test = amexCard(userSpendInput);
+
+   console.log(test[0]);
+   console.log(test[1]);
+   console.log(test[2]);
+   //console.log(amexCard(userSpendInput));
+   //citiCard(userSpendInput);
+   // jetBlueCard(userSpendInput);
+   // console.log(userSpendInput);
+
+   $("tbody").append(`<tr><th>1</th><td>Amex</td><td>${test[0]}</td><td>${test[1]}</td><td>${test[2]}</td></tr>`);
+
+   // <tr>
+   //              <th scope="row">1</th>
+   //              <td>Amex</td>
+   //              <td>200</td>
+   //              <td>5x</td>
+   //              <td>4%</td>
+   //          </tr>
+
+
+
 });
