0f27481f01446f2accf9612af646a23eb8da763b
Skyler R <skyler.rencher@gmail.com>
2020-11-17 02:05:09 -0500

    updated the calculate.js to have the functions that work with user input from the calculator modal.

--- public/assets/js/calculate.js
+++ public/assets/js/calculate.js
@@ -1,104 +1,93 @@
-// const { values } = require("sequelize/types/lib/operators");
-
 const userSpendBtn = $("#text-enter-button");
 
 const spendAmountSaved = JSON.parse(localStorage.getItem("spentAmount")) || [];
 
 // =======================================================================
 
+// AMEX Card Function that calculates the perks 
 const amexCard = function (num1) {
-   const cashBack = function (num1) {
+   const cashBack = function() {
       const num2 = .02;
       const result = num1 * num2;
       console.log("$" + result.toFixed(2));
       return result.toFixed(2);
    };
-   cashBack(num1);
 
-   const rewardsEarned = function (num1) {
+   const rewardsEarned = function () {
       const num2 = .75;
       const result = num1 * num2;
       console.log(result.toFixed(2) + " points");
       return result.toFixed(2);
    };
-   rewardsEarned(num1);
 
-   const milesEarned = function (num1) {
+   const milesEarned = function () {
       const num2 = 1.25;
       const result = num1 * num2;
       console.log(result.toFixed(2) + " miles");
       return result.toFixed(2);
    };
-   milesEarned(num1);
 
+   return [cashBack(), rewardsEarned(), milesEarned()];
 
 };
 
-// amexCard(1000);
-
+// Citi Card Function that calculates the perks 
 const citiCard = function (num1) {
-   const cashBack = function (num1) {
+   const cashBack = function () {
       const num2 = .04;
       const result = num1 * num2;
       console.log("$" + result.toFixed(2));
       return result.toFixed(2);
    };
-   cashBack(num1);
 
-   const rewardsEarned = function (num1) {
+   const rewardsEarned = function () {
       const num2 = .80;
       const result = num1 * num2;
       console.log(result.toFixed(2) + " points");
       return result.toFixed(2);
    };
-   rewardsEarned(num1);
 
-   const milesEarned = function (num1) {
+   const milesEarned = function () {
       const num2 = 1.10;
       const result = num1 * num2;
       console.log(result.toFixed(2) + " miles");
       return result.toFixed(2);
    };
-   milesEarned(num1);
 
+   return [cashBack(), rewardsEarned(), milesEarned()];
 
 };
 
-// citiCard(600);
-
+// Citi Card Function that calculates the perks 
 const jetBlueCard = function (num1) {
-   const cashBack = function (num1) {
+   const cashBack = function () {
       const num2 = .03;
       const result = num1 * num2;
       console.log("$" + result.toFixed(2));
       return result.toFixed(2);
    };
-   cashBack(num1);
 
-   const rewardsEarned = function (num1) {
+   const rewardsEarned = function () {
       const num2 = .85;
       const result = num1 * num2;
       console.log(result.toFixed(2) + " points");
       return result.toFixed(2);
    };
-   rewardsEarned(num1);
 
-   const milesEarned = function (num1) {
+   const milesEarned = function () {
       const num2 = 1.15;
       const result = num1 * num2;
       console.log(result.toFixed(2) + " miles");
       return result.toFixed(2);
    };
-   milesEarned(num1);
 
+   return [cashBack(), rewardsEarned(), milesEarned()];
 
 };
 
-// jetBlueCard(4000);
-
 // ================================================================
 
-// This function handles events where one button is clicked
+// This function initiates the calculating functions for each card upon each click
 const cardPerkCalc = $(userSpendBtn).on("click", (event) => {
    event.preventDefault();
 
@@ -121,10 +110,7 @@ const cardPerkCalc = $(userSpendBtn).on("click", (event) => {
    jetBlueCard(userSpendInput);
    console.log(userSpendInput);
 
-
-
-
 });
 
-module.exports = amexCard, citiCard, jetBlueCard, cardPerkCalc;
+//module.exports = amexCard, citiCard, jetBlueCard, cardPerkCalc;
 
